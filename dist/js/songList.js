35ac69e831b259a46066daae8d6b6bbad0607302
戴江涛 <captaininphw@gmail.com>
2018-02-21 12:30:07 +0800

    songList.js

--- dist/js/songList.js
+++ dist/js/songList.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 14);
+/******/ 	return __webpack_require__(__webpack_require__.s = 16);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 14:
+/***/ 16:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -73,15 +73,164 @@
 
 /*
  * song-list.js
- * Copyright (C) 2018 daijt <daijt@david.local>
+ * Copyright (C) 2018 daijt
  *
  * Distributed under terms of the MIT license.
  */
-
 (function () {
-  'use strict';
+    'use strict';
 
-  console.log('引入 song-list.js 成功！');
+    // console.log('引入 song-list.js 成功！');
+
+    var view = {
+        el: $('.song-list > ul'),
+        render: function render(datas) {
+            var _this = this;
+
+            datas.map(function (data) {
+                _this.el.prepend(data);
+            });
+        },
+        active: function active(dom) {
+            this.el.find('li').map(function (index, domLi) {
+                $(domLi).removeClass('active');
+            });
+            if (dom) {
+                $(dom).addClass('active');
+            }
+        },
+        blink: function blink(selector) {
+            var _this2 = this;
+
+            this.el.find(selector).addClass('blink');
+            setTimeout(function () {
+                if (_this2.el.find(selector)) {
+                    _this2.el.find(selector).removeClass('blink');
+                }
+            }, 4000);
+        },
+        deleteElement: function deleteElement(element) {
+            var _this3 = this;
+
+            setTimeout(function () {
+                _this3.el.find(element).remove();
+            }, 3000);
+        },
+        fetchCurrentLiDom: function fetchCurrentLiDom(data) {
+            return '#' + data;
+        },
+        clearCurrentLiDom: function clearCurrentLiDom(selector) {
+            view.el.find(selector).empty();
+        },
+        updateCurrentLiDom: function updateCurrentLiDom(selector, templates) {
+            var _this4 = this;
+
+            templates.map(function (template) {
+                _this4.el.find(selector).prepend(template);
+            });
+        }
+    };
+
+    var model = {
+        data: {},
+        template: '\n            <li id="{{id}}">\n                <p class="song-name" title="{{song}}"><a href="http://{{url}}" target="_blank">{{song}}</a></p>\n                <p class="singer" title="{{singer}}"><i class="iconfont icon-geshou"></i>{{singer}}</p>\n            </li>\n        ',
+        templateSong: '<p class="song-name" title="{{song}}"><a href="http://{{url}}" target="_blank">{{song}}</a></p>',
+        templateSinger: '<p class="singer" title="{{singer}}"><i class="iconfont icon-geshou"></i>{{singer}}</p>',
+        temporaryTemplate: [],
+        refreshData: function refreshData(data) {
+            Object.assign(this.data, JSON.parse(JSON.stringify(data)));
+        },
+        generateTemporaryTemplate: function generateTemporaryTemplate(temporaryTemplate, data) {
+            for (var key in data) {
+                switch (key) {
+                    case 'id':
+                        temporaryTemplate = temporaryTemplate.replace(/\{\{id\}\}/g, data[key]);
+                        break;
+                    case 'song':
+                        temporaryTemplate = temporaryTemplate.replace(/\{\{song\}\}/g, data[key]);
+                        break;
+                    case 'singer':
+                        temporaryTemplate = temporaryTemplate.replace(/\{\{singer\}\}/g, data[key]);
+                        break;
+                    case 'url':
+                        temporaryTemplate = temporaryTemplate.replace(/\{\{url\}\}/g, data[key]);
+                        break;
+                }
+            }
+            return temporaryTemplate;
+        },
+        fetchLeanCloudData: function fetchLeanCloudData() {
+            var _this5 = this;
+
+            var query = new AV.Query('SongList');
+            return query.find().then(function (songs) {
+                model.data = songs.map(function (song) {
+                    return {
+                        id: song.id,
+                        song: song.attributes.song,
+                        singer: song.attributes.singer,
+                        url: song.attributes.url
+                    };
+                });
+                return _this5.showLeanCloudData();
+            }, function (error) {
+                console.log(error);
+            });
+        },
+        showLeanCloudData: function showLeanCloudData() {
+            var _this6 = this;
+
+            this.data.map(function (song) {
+                _this6.temporaryTemplate.push(_this6.generateTemporaryTemplate(_this6.template, song));
+            });
+            return this.temporaryTemplate;
+        }
+    };
+
+    var controller = {
+        init: function init() {
+            model.fetchLeanCloudData().then(function (data) {
+                view.render(data);
+            }, function (error) {
+                console.log(error);
+            });
+            this.bindEvents();
+        },
+        bindEvents: function bindEvents() {
+            EventsHub.subscribe('edited', function (data) {
+                model.refreshData(data);
+                view.render([model.generateTemporaryTemplate(model.template, model.data)]);
+                view.blink('li:first-child');
+            });
+            this.clickEvent();
+            EventsHub.subscribe('uploading', function () {
+                view.active();
+            });
+            EventsHub.subscribe('delete', function () {
+                view.blink('.active');
+                view.deleteElement('.active');
+            });
+            EventsHub.subscribe('update', function (data) {
+                model.refreshData(data);
+                view.blink(view.fetchCurrentLiDom(model.data.id));
+                view.clearCurrentLiDom(view.fetchCurrentLiDom(model.data.id));
+                view.updateCurrentLiDom(view.fetchCurrentLiDom(model.data.id), [model.generateTemporaryTemplate(model.templateSinger, model.data), model.generateTemporaryTemplate(model.templateSong, model.data)]);
+            });
+        },
+        clickEvent: function clickEvent() {
+            view.el.on('click', 'li', function (event) {
+                view.active(event.currentTarget);
+                EventsHub.publish('modify', {
+                    id: event.currentTarget.id,
+                    song: event.currentTarget.firstElementChild.firstElementChild.textContent,
+                    singer: event.currentTarget.lastElementChild.textContent,
+                    url: event.currentTarget.firstElementChild.firstElementChild.href.substring(7)
+                });
+            });
+        }
+    };
+
+    controller.init();
 })();
 
 /***/ })

7177abece3fdc1d3e04445ef0334093184f648fc
戴江涛 <captaininphw@gmail.com>
2018-02-21 12:29:46 +0800

    profile.js

---
+++ dist/js/profile.js
@@ -0,0 +1,107 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 13);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 13:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/*
+ * profile.js
+ * Copyright (C) 2018 daijt
+ *
+ * Distributed under terms of the MIT license.
+ */
+(function () {
+    'use strict';
+
+    // console.log('引入 profile.js 成功！');
+
+    var view = {
+        el: $('.top-bar'),
+        render: function render(data) {
+            this.el.append(data);
+        }
+    };
+
+    var model = {
+        template: '\n            <div class="button"></div>\n            <div class="logo"></div>\n            <ul class="profile">\n                <li><i class="iconfont icon-person"></i>\u6234\u6C5F\u6D9B</li>\n                <li><i class="iconfont icon-wechat"></i>qq1319836729</li>\n                <li><i class="iconfont icon-tel02"></i>18393986458</li>\n                <li><i class="iconfont icon-mail"></i>captaininphw@gmail.com</li>\n                <li><i class="iconfont icon-blog"></i><a href="http://captaininphw.xyz">Blog</a></li>\n                <li><i class="iconfont icon-github"></i><a href="https://github.com/CaptainInPHW/NeteaseMusic">GitHub</a></li>\n            </ul>\n        '
+    };
+
+    var controller = {
+        init: function init() {
+            view.render(model.template);
+        }
+    };
+
+    controller.init();
+})();
+
+/***/ })
+
+/******/ });

a3293922db763d22285b881abc6a48c0b1e85faa
戴江涛 <captaininphw@gmail.com>
2018-02-21 12:26:08 +0800

    admin.js

--- src/javascript/admin.js
+++ src/javascript/admin.js
@@ -1,6 +1,6 @@
 /*
  * admin.js
- * Copyright (C) 2018 daijt <daijt@david.local>
+ * Copyright (C) 2018 daijt
  *
  * Distributed under terms of the MIT license.
  */
@@ -10,10 +10,16 @@ import '../style/admin.scss';
 (function () {
     'use strict';
 
-    console.log('引入 admin.js 成功！');
+    // console.log('引入 admin.js 成功！');
 
     const appId = '3lYwUmOrkdkkv4zmxHzDbp3w-gzGzoHsz';
     const appKey = '5KWyRdYSGUIjOYDMirAnfDJ4';
     AV.init({ appId, appKey });
 
+    let mySwiper = new Swiper ('.swiper-container', {
+        autoplay: {
+            delay: 5000,
+        },
+        effect: 'fade',
+    })
 })();

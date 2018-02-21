a91013c4442e8072501da0837a0681dc2c57c1ad
戴江涛 <captaininphw@gmail.com>
2018-02-21 12:28:18 +0800

    welcome.js

---
+++ src/javascript/welcome.js
@@ -0,0 +1,66 @@
+/*
+ * welcome.js
+ * Copyright (C) 2018 daijt
+ *
+ * Distributed under terms of the MIT license.
+ */
+(function () {
+    'use strict';
+
+    // console.log('引入 welcome.js 成功！');
+
+    let view = {
+        el: $('.welcome-page'),
+        render(data) {
+            this.el.append(data);
+        },
+        show() {
+            this.el.removeClass('hide');
+        },
+        hide() {
+            this.el.addClass('hide');
+        }
+    };
+
+    let model = {
+        template: `
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide"></div>
+                    <div class="swiper-slide"></div>
+                    <div class="swiper-slide"></div>
+                    <div class="swiper-slide"></div>
+                    <div class="swiper-slide"></div>
+                    <div class="swiper-slide"></div>
+                    <div class="swiper-slide"></div>
+                    <div class="swiper-slide"></div>
+                </div>
+            </div>
+            <p class="welcome-info"><i class="iconfont icon-CN_NetEasemusic"></i>欢迎使用网易云音乐后台管理系统！</p>
+        `
+    };
+
+    let controller = {
+        init() {
+            view.render(model.template);
+            this.bindEvents();
+        },
+        bindEvents() {
+            EventsHub.subscribe('delete', () => {
+                view.show();
+            });
+            EventsHub.subscribe('modify', () => {
+                view.hide();
+            });
+            EventsHub.subscribe('new', () => {
+                view.hide();
+            });
+            EventsHub.subscribe('edited', () => {
+                view.show();
+            });
+        }
+    };
+
+    controller.init();
+
+})();

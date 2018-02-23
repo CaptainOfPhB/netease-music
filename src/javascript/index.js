9df2494767c07548073304d3737cd82281cbf993
戴江涛 <captaininphw@gmail.com>
2018-02-23 17:55:09 +0800

    index.js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,14 +1,22 @@
 /*
  * index.js
- * Copyright (C) 2018 daijt <daijt@david.local>
+ * Copyright (C) 2018 daijt
  *
  * Distributed under terms of the MIT license.
  */
-
+import '../style/normalize.css';
 import '../style/index.scss';
 
-(function(){
-  'use strict';
-  console.log('index.js');
+(function () {
+    'use strict';
+
+    // 设置 REM
+    $('head').prepend(`
+        <style>
+            html {
+                font-size: ${window.innerWidth / 20}px;
+            }
+        </style>
+    `);
 
 })();

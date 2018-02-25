25c5ec4b747365501f55b893ac8b61b9b5f687af
戴江涛 <captaininphw@gmail.com>
2018-02-25 13:24:23 +0800

    songNew.js

--- dist/js/songNew.js
+++ dist/js/songNew.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 17);
+/******/ 	return __webpack_require__(__webpack_require__.s = 19);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 17:
+/***/ 19:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";

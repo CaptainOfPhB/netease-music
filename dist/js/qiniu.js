c6241dcf34d204b66728292167fe8b9b698baf39
戴江涛 <captaininphw@gmail.com>
2018-03-01 15:07:12 +0800

    qiniu.js

--- dist/js/qiniu.js
+++ dist/js/qiniu.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 10);
+/******/ 	return __webpack_require__(__webpack_require__.s = 17);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 10:
+/***/ 17:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";

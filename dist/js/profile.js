8964c76a1f2577b6da2cbf0cd7d35cd33989eacd
戴江涛 <captaininphw@gmail.com>
2018-03-01 15:09:30 +0800

    profile.js

--- dist/js/profile.js
+++ dist/js/profile.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 15);
+/******/ 	return __webpack_require__(__webpack_require__.s = 21);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 15:
+/***/ 21:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";

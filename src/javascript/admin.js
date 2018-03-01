974ff9ca196e689e4b2d886a6734b231f7470c58
戴江涛 <captaininphw@gmail.com>
2018-03-01 15:10:23 +0800

    admin.js

--- src/javascript/admin.js
+++ src/javascript/admin.js
@@ -10,8 +10,6 @@ import '../style/admin.scss';
 (function () {
     'use strict';
 
-    // console.log('引入 admin.js 成功！');
-
     const appId = '3lYwUmOrkdkkv4zmxHzDbp3w-gzGzoHsz';
     const appKey = '5KWyRdYSGUIjOYDMirAnfDJ4';
     AV.init({ appId, appKey });

974f2ac967fedaba08a694499010ac510d533bcd
戴江涛 <captaininphw@gmail.com>
2018-03-04 21:13:37 +0800

    songNew.js

--- dist/js/songNew.js
+++ dist/js/songNew.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 25);
+/******/ 	return __webpack_require__(__webpack_require__.s = 28);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 25:
+/***/ 28:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
